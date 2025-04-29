ecc3c550c77b2092aafd6b8bd8bc5457430e671e
OKeylo <zabaev228@gmail.com>
2025-04-29 16:44:51 +0500

    feat: add lesson2.4_step8.py file

---
+++ lesson2.4_step8.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # говорим Selenium проверять в течение 12 секунд, пока цена не станет 100
+    WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), "100")
+        )
+    
+    button_book = browser.find_element(By.CSS_SELECTOR, "#book")
+    button_book.click()
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "#solve")
+    button.click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
